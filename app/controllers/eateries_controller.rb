17632c5d4af2f595b68e1ac3d84b46c54f4501da
Yuichi Kato <yuik2525@gmail.com>
2019-05-17 17:27:39 +0900

    ユーザー登録

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -6,11 +6,6 @@ class EateriesController < ApplicationController
     eatery = Eatery.new(eatery_params)
     eatery.save
     flash[:success] = "#{eatery.name}を追加しました"
-    flash[:success] = "#{eatery.address}を追加しました"
-    flash[:success] = "#{eatery.tel}を追加しました"
-    flash[:success] = "#{eatery.regular_holiday}を追加しました"
-    flash[:success] = "#{eatery.open}を追加しました"
-    flash[:success] = "#{eatery.close}を追加しました"
     redirect_to root_path
   end
 
