d28b5474df67a07f88c598d2a9747469bf52d8e7
Yuichi Kato <yuik2525@gmail.com>
2019-05-17 15:33:21 +0900

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Eatery.create(name: "七福軒", address: "〒305-0005 茨城県つくば市天久保１丁目６−１４", tel: "029-893-4799", regular_holiday: "日曜日", open:"18:00", close: "22:00")
 Eatery.create(name: "むじゃき", address: "〒305-0005 茨城県つくば市天久保２丁目１０−１９", tel: "029-896-8988", regular_holiday: "日曜日", open:"18:00", close: "22:00")
+Eatery.create(name: "天地", address: "〒305-0005 茨城県つくば市天久保２丁目６−２６", tel: "029-860-7710", regular_holiday: "なし", open:"17:30", close: "22:00")
+Eatery.create(name: "ZEYO.", address: "〒305-0005 茨城県つくば市天久保２丁目６−１ ベスト ランド Ⅳ １Ｆ", tel: "029-854-1778", regular_holiday: "なし", open:"18:00", close: "23:00")
+Eatery.create(name: "銀のしずく", address: "〒305-0005 茨城県つくば市天久保２丁目６−１", tel: "029-886-7615", regular_holiday: "火曜日", open:"17:00", close: "25:00")
